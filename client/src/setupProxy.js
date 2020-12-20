14a9eb0c949f3379e2caf8edd95a80c551c96ab5
Shuvam Nandi <shuvamnandi9@gmail.com>
2020-12-20 13:17:22 +0800

    Added comment in client side setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
   app.use(
     ["/api", "/auth/google"],
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "http://localhost:5000", // needed only for development as there are different servers for client and server
     })
   );
 };
